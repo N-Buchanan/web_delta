8d0ecc7c9fae94694f6eacb307ff4305d42145e9
Nolan Buchanan <ncbuchan@gmail.com>
2017-05-31 17:26:37 -0400

    updated setup.py to reflect latest version

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name = 'web_delta',
+        packages = ['web_delta'],
+        version = '0.1.1',
+        description = 'A batch web scraping library designed for use with frequently updating sites',
+        author = 'Nolan Buchanan',
+        author_email = 'ncbuchan@gmail.com',
+        url = 'https://github.com/N-Buchanan/web_delta', #github
+        download_url = 'https://github.com/N-Buchanan/web_delta/archive/0.1.1.tar.gz',
+        keywords = ['web', 'scraping', 'delta', 'changes'],
+        classifiers = []
+        )
